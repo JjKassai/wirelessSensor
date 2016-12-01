58019996c4a452881e7b89baf5901cf9f718196b
Josef Kassai <JKassai@aerotech.com>
2016-12-01 16:42:44 -0500

    Started the MPLAB project and added some skeleton source files

---
+++ 5. MPLAB/source/main.c
@@ -0,0 +1,13 @@
+// fuseConfiguration must be included before any other project includes
+#include "fuseConfiguration.h"
+
+#include <xc.h>
+
+void main(void) 
+{
+    while(1)
+    {
+        // This space intentionally left blank
+    }
+    return;
+}
