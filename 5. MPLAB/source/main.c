546398aa6d3b6b4cfb816c7c03cdad04d38358b6
Josef Kassai <JKassai@aerotech.com>
2016-12-01 16:43:17 -0500

    More from previous check-in I forgot to include

--- 5. MPLAB/source/main.c
+++ 5. MPLAB/source/main.c
@@ -2,9 +2,15 @@
 #include "fuseConfiguration.h"
 
 #include <xc.h>
+#include "peripheralConfiguration.h"
 
 void main(void) 
 {
+    configureClock();
+    configureGPIO();
+    configureUART();
+    configureI2C();
+    
     while(1)
     {
         // This space intentionally left blank
--- 5. MPLAB/source/peripheralConfiguration.h
+++ 5. MPLAB/source/peripheralConfiguration.h
@@ -1,28 +1,30 @@
-#ifndef FUSECONFIGURATION_H
-#define	FUSECONFIGURATION_H
+#ifndef PERIPHERALCONFIGURATION_H
+#define	PERIPHERALCONFIGURATION_H
 
-// CONFIG1
-#pragma config FOSC = INTOSC    // Oscillator Selection (INTOSC oscillator: I/O function on CLKIN pin)
-#pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
-#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
-#pragma config MCLRE = OFF      // MCLR Pin Function Select (MCLR/VPP pin function is digital input)
-#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
-#pragma config CPD = OFF        // Data Memory Code Protection (Data memory code protection is disabled)
-#pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
-#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
-#pragma config IESO = ON        // Internal/External Switchover (Internal/External Switchover mode is enabled)
-#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is enabled)
-
-// CONFIG2
-#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
-#pragma config PLLEN = ON       // PLL Enable (4x PLL enabled)
-#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
-#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
-#pragma config LVP = ON         // Low-Voltage Programming Enable (Low-voltage programming enabled)
-
-// #pragma config statements should precede project file includes.
-// Use project enums instead of #define for ON and OFF.
+void configureClock(void);
+void configureGPIO(void);
+void configureI2C(void);
+void configureUART(void);
 
 
-#endif	/* FUSECONFIGURATION_H */
+void configureClock(void)
+{
+    OSCCONbits.IRCF = 0b1111;
+}
 
+void configureGPIO(void)
+{
+    
+}
+
+void configureI2C(void)
+{
+    
+}
+
+void configureUART(void)
+{
+    
+}
+
+#endif	/* PERIPHERALCONFIGURATION_H */
